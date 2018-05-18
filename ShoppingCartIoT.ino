a20c4c1f535384ae711343112bb3696e0b9f55ab
Unknown <icbjayasinghe@gmail.com>
2018-05-18 20:18:59 +0530

    initial commit

---
+++ ShoppingCartIoT.ino
@@ -0,0 +1,30 @@
+#include <ArduinoJson.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiServer.h>
+#include <WiFiServerSecure.h>
+#include <WiFiUdp.h>
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin("Home ZTE","Shift5771");
+
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.println("Waiting for connection");
+    }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
